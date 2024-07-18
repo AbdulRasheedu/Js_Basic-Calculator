9fc0999b006b719bbdba6b29960d32a662162375
AbdulRasheedu <131258568+AbdulRasheedu@users.noreply.github.com>
2024-07-18 20:59:42 +0530

    "commit"

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-function appendCharecter (charecter){
-    document.getElementById("display").value += charecter;
-    document.body.style.backgroundColor = "red";
+function appendCharecter(charecter) {
+  document.getElementById("display").value += charecter;
+  document.body.style.backgroundColor = "lightblue";
 }
-function clearDisplay(){
-    document.getElementById("display").value ='';
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+  document.body.style.backgroundColor = "lightcoral";
 }
-function clearEntry(){
-    const display = document.getElementById("display");
-    display.value = display.value.slice(0,-1);
+function clearEntry() {
+  const display = document.getElementById("display");
+  display.value = display.value.slice(0, -1);
+  document.body.style.backgroundColor = "lightgreen";
 }
-function calculatorResult(){
-    const display = document.getElementById("display");
-    try{
-        display.value = eval(display.value);
-    }
-    catch(e){
-        display.value = "Error";
-    }
+function calculatorResult() {
+  const display = document.getElementById("display");
+  try {
+    display.value = eval(display.value);
+  } catch (e) {
+    display.value = "Error";
+  }
 }
